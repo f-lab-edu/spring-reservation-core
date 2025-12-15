85825286eb406110820e16bedd6f59dae5f08a37
상진 <62305110+xxx-sj@users.noreply.github.com>
2025-12-15 18:04:18 +0900

    chore: errorCode status 변경
    - HttpStatus의 value 사용

--- src/main/java/com/example/reservation/reservationsystem/domain/slot/exception/SlotErrorCode.java
+++ src/main/java/com/example/reservation/reservationsystem/domain/slot/exception/SlotErrorCode.java
@@ -3,13 +3,14 @@ package com.example.reservation.reservationsystem.domain.slot.exception;
 import com.example.reservation.reservationsystem.global.error.exception.ErrorCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
 public enum SlotErrorCode implements ErrorCode {
-    SLOT_NOT_RESERVABLE("S001", "예약할 수 없습니다.", 409),
-    SLOT_ALREADY_FULL("S002", "슬롯이 이미 꽉 찼습니다.", 409),
-    SLOT_NOT_FOUND("S003", "슬롯을 찾을 수 없습니다.", 404);
+    SLOT_NOT_RESERVABLE("S001", "예약할 수 없습니다.", HttpStatus.CONFLICT.value()),
+    SLOT_ALREADY_FULL("S002", "슬롯이 이미 꽉 찼습니다.", HttpStatus.CONFLICT.value()),
+    SLOT_NOT_FOUND("S003", "슬롯을 찾을 수 없습니다.", HttpStatus.NOT_FOUND.value());
 
     private final String code;
     private final String message;
