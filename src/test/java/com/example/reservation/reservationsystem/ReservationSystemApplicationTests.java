61578dff4c1ecb924f36d49f98af0988c1643584
상진 <62305110+xxx-sj@users.noreply.github.com>
2025-12-19 11:04:15 +0900

    fix: 소나큐브 오류 수정

--- src/test/java/com/example/reservation/reservationsystem/ReservationSystemApplicationTests.java
+++ src/test/java/com/example/reservation/reservationsystem/ReservationSystemApplicationTests.java
@@ -1,6 +1,13 @@
 package com.example.reservation.reservationsystem;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ReservationSystemApplicationTests { }
+class ReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
